3f18f1a92ee18678ff928b282b36105f47100581
Sandra-Louchart <sandra.louchart@gmail.com>
2020-11-30 08:47:21 +0100

    Finish quest 03

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,8 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
     }
 }
